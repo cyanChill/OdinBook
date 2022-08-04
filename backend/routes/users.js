5b0746ce7c085d0c506efd0a6f2f9595b68dfe4c
cyanChill <anthonyliang9@gmail.com>
2022-08-04 19:57:07 -0400

    updated logic flow

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,13 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const passport = require("passport");
 
 const routeMiddleware = require("../utils/routeMiddleware");
 const usersController = require("../controllers/usersController");
 
 /* ❗ Middlewares ❗ */
-// User must be logged in to access this route
-router.use(passport.authenticate("jwt", { session: false }));
 // :userId parameter must link to a valid user
 router.use("/:userId", routeMiddleware.validUserId);
 // Given the :userId is valid, check if viewer is the friend or owner of :userId
@@ -17,7 +14,7 @@ router.use("/:userId", routeMiddleware.isFriendOrOwner);
 // GET All users (returns basic information such as name & profile picture)
 router.get("/", usersController.allUsersGet);
 
-// GET user info
+// GET user info (different values depending on viewing user)
 router.get("/:userId", usersController.userGet);
 
 // PUT route for updating user profile (updater must own profile)
@@ -32,6 +29,13 @@ router.put(
   "/:userId/profilepic",
   routeMiddleware.isProfileOwner,
   usersController.updateProfilePicPut
+);
+
+// DELETE route for deleting user
+router.delete(
+  "/:userId",
+  routeMiddleware.isProfileOwner,
+  usersController.userDelete
 );
 
 module.exports = router;
--- backend/utils/routeMiddleware.js
+++ backend/utils/routeMiddleware.js
@@ -1,4 +1,3 @@
-const { readToken } = require("./jwt");
 const User = require("../models/User");
 
 /* 
@@ -21,28 +20,24 @@ exports.validUserId = async (req, res, next) => {
 
 // Checks if current user is a "friend" of :userId user (assuming :userId user
 // exists) or is the user with :userId
-exports.isFriendOrOwner = [
-  // Set req.userId to "id" value found in JWT token
-  readToken,
-  async (req, res, next) => {
-    try {
-      const user = await User.findOne({
-        _id: req.params.userId,
-        friends: req.userId,
-      });
-      if (req.userId === req.params.userId) req.isOwner = true;
-      else req.isOwner = false;
+exports.isFriendOrOwner = async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      _id: req.params.userId,
+      friends: req.userId,
+    });
+    if (req.userId === req.params.userId) req.isOwner = true;
+    else req.isOwner = false;
 
-      if (user || req.userId === req.params.userId) req.isFriendOrOwner = true;
-      else req.isFriendOrOwner = false;
-      return next();
-    } catch (err) {
-      return res.status(500).json({
-        message: "Something went wrong while verifying the userId.",
-      });
-    }
-  },
-];
+    if (user || req.userId === req.params.userId) req.isFriendOrOwner = true;
+    else req.isFriendOrOwner = false;
+    return next();
+  } catch (err) {
+    return res.status(500).json({
+      message: "Something went wrong while verifying the userId.",
+    });
+  }
+};
 
 // Checks if userId of accessor of route is equal to :userId of route
 exports.isProfileOwner = async (req, res, next) => {
