4bb185aa5be50bb817eb35ef3517d5ebbaa11119
cyanChill <anthonyliang9@gmail.com>
2022-08-12 21:06:07 -0400

    fix build error from firebase admin

--- backend/utils/firebaseAdminConfig.js
+++ backend/utils/firebaseAdminConfig.js
@@ -1,8 +1,6 @@
 const { initializeApp, cert } = require("firebase-admin/app");
 const { getStorage } = require("firebase-admin/storage");
 
-const serviceAccount = require("../serviceAccountKey.json");
-
 initializeApp({
   credential: cert({
     project_id: process.env.FIREBASE_PROJECT_ID,
