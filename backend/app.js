5a28c33ebd9aa54aabb13f48797d376ca7c6f85c
cyanChill <anthonyliang9@gmail.com>
2022-08-12 20:53:37 -0400

    updated backend for production

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const compression = require("compression");
 const cors = require("cors");
 
 require("./utils/firebaseAdminConfig");
@@ -21,6 +22,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // Middleware
+app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
