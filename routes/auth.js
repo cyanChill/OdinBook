afd848d737e81114ffa27c9ce45ab98caf6fbc97
cyanChill <anthonyliang9@gmail.com>
2022-08-14 17:50:46 -0400

    add basic rate limitting on login + signup

--- routes/auth.js
+++ routes/auth.js
@@ -1,36 +1,29 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
+const rateLimit = require("express-rate-limit");
 
 const authController = require("../controllers/authController");
+
+const limiterSignup = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10, // Limit each IP to 10 requests per 'window'
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+const limiterLogin = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 5, // Limit each IP to 5 requests per 'window'
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 
 // ⭐ Current Route: "/api/auth" ⭐
 
 /* Handle user signups */
+router.use("/signup", limiterSignup);
 router.post("/signup", authController.signup);
-
-/* Handle non-Facebook logins */
-router.post("/login", authController.normalLogin);
-
-/* Handle Facebook logins */
-router.get(
-  "/login/facebook",
-  passport.authenticate("facebook", { scope: ["email"], session: false })
-);
-
-router.get(
-  "/facebook/redirect",
-  passport.authenticate("facebook", {
-    scope: ["email"],
-    session: false,
-    failureRedirect: `${process.env.FRONTEND_URL}`,
-    failureMessage: true,
-  }),
-  authController.facebookLogin
-);
-
-/* Login as Demo Account */
-router.get("/login/demo", authController.demoLogin);
 
 /* Verify if JWT token is still valid */
 router.get(
@@ -49,5 +42,31 @@ router.get(
     });
   }
 );
+
+/* Handle Facebook logins */
+router.get(
+  "/login/facebook",
+  passport.authenticate("facebook", { scope: ["email"], session: false })
+);
+
+router.get(
+  "/facebook/redirect",
+  passport.authenticate("facebook", {
+    scope: ["email"],
+    session: false,
+    failureRedirect: `${process.env.FRONTEND_URL}`,
+    failureMessage: true,
+  }),
+  authController.facebookLogin
+);
+
+/* Apply this rate limiter to the "regular" login routes below */
+router.use(limiterLogin);
+
+/* Handle non-Facebook logins */
+router.post("/login", authController.normalLogin);
+
+/* Login as Demo Account */
+router.get("/login/demo", authController.demoLogin);
 
 module.exports = router;
